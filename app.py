5b7ee6b308acf8ac4d86e2c98f0cf81bc8aede90
Melanie Mai <melaniesmai@outlook.com>
2022-12-03 02:53:07 -0500

    update app

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
